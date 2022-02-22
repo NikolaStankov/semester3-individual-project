22dfa4c9d3870808403a12740345adac5f747f61
Stankov,Nikola N.M <n.minkov@student.fontys.nl>
2022-02-22 13:23:18 +0200

    Added a SpringTest annotation

--- IndividualTrack/src/test/java/fontys/sem3/individual_track/model/ExampleHelloTest.java
+++ IndividualTrack/src/test/java/fontys/sem3/individual_track/model/ExampleHelloTest.java
@@ -2,9 +2,11 @@ package fontys.sem3.individual_track.model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class ExampleHelloTest {
 
     ExampleHello exampleHello;
